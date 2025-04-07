56daafdde0014da3b65172caacf0cbfc1962542a
Eduardo Ballesteros <eduardo.ballesterosg@outlook.com>
2025-04-07 19:11:50 +0200

    dynamic __version__

--- dloop/__init__.py
+++ dloop/__init__.py
@@ -1,9 +1,12 @@
-__version__ = "0.1.0"
+import importlib.metadata
 
 # Import key classes
 from .events import Event, LoopEvents
 from .loop import Loop
 from .types import LoopState
+
+# figure out version dynamically
+__version__ = importlib.metadata.version("dloop")
 
 # Define public API
 __all__ = ["Event", "LoopEvents", "Loop", "LoopState"]
